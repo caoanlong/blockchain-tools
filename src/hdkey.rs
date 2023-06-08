d63e871742fb6a9aa75e6c61e93b46732849355d
Vic <vic.unity@skig.tech>
2023-06-08 11:05:15 +0800

    tron address

--- src/hdkey.rs
+++ src/hdkey.rs
@@ -1,16 +1,32 @@
+use std::io::Read;
 use std::ops::Deref;
+use std::ptr::null;
 use std::str::FromStr;
-use bip32::{ExtendedPrivateKey, PrivateKey, XPrv, DerivationPath as DPath};
-use bip32::secp256k1::ecdsa::SigningKey;
-use bip39::{Seed, Mnemonic, Language};
+use bip32::{ExtendedPrivateKey, PrivateKey, PublicKey, XPrv, DerivationPath as DPath};
+use bip32::secp256k1::ecdsa::{SigningKey, VerifyingKey};
+use bip32::secp256k1::elliptic_curve::sec1::{CompressedPoint, ToEncodedPoint};
+use bip32::secp256k1::sha2::Sha256;
+use bip39::{Mnemonic, Seed, Language};
 use derivation_path::DerivationPath;
 use sha3::{Digest, Keccak256};
+use ripemd::{Ripemd160};
 
 pub enum ChainType {
     BTC = 0,
     LTC = 2,
     ETH = 60,
     TRX = 195,
+}
+
+impl ToString for ChainType {
+    fn to_string(&self) -> String {
+        match self {
+            ChainType::BTC => String::from("0"),
+            ChainType::LTC => String::from("2"),
+            ChainType::ETH => String::from("60"),
+            ChainType::TRX => String::from("195"),
+        }
+    }
 }
 
 pub struct HDKey {
@@ -20,22 +36,12 @@ pub struct HDKey {
 }
 
 impl HDKey {
-    // pub fn from_seed(seed: Seed) -> Self {
-    //     let root_xprv = XPrv::new(&seed)?;
-    //     assert_eq!(root_xprv, XPrv::derive_from_path(&seed, &"m".parse()?)?);
-    //
-    //     HDKey {
-    //         seed,
-    //         mnemonic: NULL,
-    //         root_xprv
-    //     }
-    // }
 
     pub fn from_mnemonic(m: String) -> Self {
         let result = Mnemonic::from_phrase(m.as_str(), Language::English);
         let mnemonic = result.unwrap();
         let seed = Seed::new(&mnemonic, "");
-        let root_xprv = XPrv::new(&seed).unwrap();
+        let root_xprv = XPrv::new(seed.clone()).unwrap();
         HDKey {
             seed,
             mnemonic,
@@ -43,23 +49,35 @@ impl HDKey {
         }
     }
 
-    pub fn generate(&self, chain_type: ChainType, index: u32) -> &[u8] {
-        let path = DerivationPath::bip44(44, chain_type as u32, 0, index).unwrap();
-        let p = DPath::from_str(path.to_string().as_str()).unwrap();
+    pub fn generate(&self, chain_type: ChainType, index: u32) -> Vec<u8> {
+        // let child_path = "m/44'/195'/0'/0/0";
+        let path = DerivationPath::bip44(chain_type as u32, 0, 0, index).unwrap();
+        println!("[path]:{}", path.to_string());
+        let p = DPath::from_str(&path.to_string()).unwrap();
         let child_xprv = XPrv::derive_from_path(&self.seed, &p).unwrap();
         let private_key = child_xprv.private_key();
-        let compressed_point = private_key.public_key().to_bytes();
-        let pub_key_bytes = compressed_point.clone();
-        // let pub_key = hex::encode(pub_key_bytes);
-        // println!("[len]:{}", pub_key.len());
-        &pub_key_bytes.as_slice()
+        println!("[priv_key]:{}", hex::encode(private_key.to_bytes().clone()));
+        // 97c2ff0e159ff11d70164c9d210b60a1e23fcedb37cb8e0206aa817bd932692c
+        let compressed_point = private_key.clone().public_key().to_bytes();
+        let compressed_pub_key_bytes = compressed_point.clone();
+        println!("[compressed_pub_key]:{}", hex::encode(compressed_pub_key_bytes));
+        let public_key = private_key.public_key();
+        let point = public_key.to_encoded_point(false);
+        let mut pub_key_bytes = point.as_bytes();
+        pub_key_bytes = &pub_key_bytes[1..];
+        pub_key_bytes.to_vec()
     }
 
-    // pub fn gen_trx(&self, pub_key: &[u8]) -> &[u8] {
-    //     let mut hasher = Keccak256::new();
-    //     hasher.update(pub_key);
-    //     let result = hasher.finalize();
-    //     let r = &result[..];
-    //     return r
-    // }
+    pub fn gen_trx(&self, pub_key: Vec<u8>) -> String {
+        let mut hasher = Keccak256::new();
+        hasher.update(pub_key.clone());
+        let _hash = hasher.finalize();
+        let hash = &_hash[12..];
+        let mut init_address: Vec<u8>= vec![0x41];
+        init_address.extend(hash);
+        let hash2 = Sha256::digest(&Sha256::digest(&init_address).to_vec()).to_vec();
+        let check_sum = &hash2[0..4];
+        init_address.extend(check_sum);
+        return bs58::encode(init_address).into_string();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ mod mnemonic;
 mod hdkey;
 
 fn main() {
-    let mnemonic_phrase = mnemonic::create_mnemonic();
+    // let mnemonic_phrase = mnemonic::create_mnemonic();
+    let mnemonic_phrase = String::from("liar torch useful breeze vacuum release kit purpose wave carpet weekend bonus");
     let root_key = HDKey::from_mnemonic(mnemonic_phrase);
-    let pub_key = root_key.generate(ChainType::ETH, 0);
-    println!("[pub_key]: {}", hex::encode(pub_key));
-    // let address = root_key.gen_trx(pub_key);
-    // println!("[address]: {}", address.len());
+    let pub_key = root_key.generate(ChainType::TRX, 0);
+    println!("[pub_key]: {}", hex::encode(pub_key.clone()));
+    let address = root_key.gen_trx(pub_key.clone());
+    println!("[address]: {}", address);
 }
